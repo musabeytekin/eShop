e4800689ad1f8044a6c68b2055c9362cf15a9c2a
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-09-30 20:52:58 +0300

    EventBus: IntegrationEvent Model

---
+++ src/BuildingBlocks/EventBus/EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,23 @@
+using System.Text.Json.Serialization;
+
+namespace EventBus.Events;
+
+public class IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createDate)
+    {
+        Id = id;
+        CreationDate = createDate;
+    }
+
+    [JsonInclude] public DateTime CreationDate { get; set; }
+
+    [JsonInclude] public Guid Id { get; set; }
+}
