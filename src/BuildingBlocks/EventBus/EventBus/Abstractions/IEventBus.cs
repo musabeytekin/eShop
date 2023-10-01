4271eb2ff631748980ef028503b2adf3b304258a
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-10-01 17:05:40 +0300

    EventBus: Base Abstractions

---
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IEventBus.cs
@@ -0,0 +1,16 @@
+using EventBus.Events;
+
+namespace EventBus.Abstractions;
+
+public interface IEventBus
+{
+    void publish(IntegrationEvent @event);
+    
+    void subscribe<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+    
+    void unsubscribe<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+}
---
+++ src/BuildingBlocks/EventBus/EventBus/Extensions/GenericTypeExtensions.cs
@@ -0,0 +1,22 @@
+namespace EventBus.Extensions;
+
+public static class GenericTypeExtensions
+{
+    public static string GetGenericTypeName(this Type type)
+    {
+        string typeName;
+
+        if (type.IsGenericType)
+        {
+            var genericTypes = string.Join(",", type.GetGenericArguments().Select(t => t.Name).ToArray()); // example generic type = "T,TH" 
+            
+            typeName = $"{type.Name.Remove(type.Name.IndexOf('`'))}<{genericTypes}>";
+        }
+        else
+        {
+            typeName = type.Name;
+        }
+
+        return typeName;
+    }
+}
